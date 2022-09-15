71d20e78ab18f7290c2d3e3cc59771b142683f3c
dustwood132 <dustwood132@SDS-L-2BHDF63>
2022-09-15 10:55:08 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class backGround
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(backGround));
+            this.playButton = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.LightGray;
+            this.playButton.ForeColor = System.Drawing.Color.Black;
+            this.playButton.Location = new System.Drawing.Point(108, 96);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(108, 42);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // text
+            // 
+            this.text.BackColor = System.Drawing.Color.Silver;
+            this.text.ForeColor = System.Drawing.Color.Black;
+            this.text.Location = new System.Drawing.Point(105, 96);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(111, 42);
+            this.text.TabIndex = 1;
+            this.text.Text = "3";
+            this.text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // backGround
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(328, 240);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.playButton);
+            this.ForeColor = System.Drawing.Color.Bisque;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "backGround";
+            this.Text = "Sword Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label text;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace WindowsFormsApp1
+{
+    public partial class backGround : Form
+    {
+        public backGround()
+        {
+            InitializeComponent();
+            text.Visible = false;
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+          
+            
+           
+           playButton.Visible = false;
+           text.Visible = true;
+
+            text.Text = "3";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            text.Text = "2";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            text.Text = "1";
+
+            Refresh();
+            Thread.Sleep(1000);
+           
+            text.Text = "Go";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            text.Visible=false;
+
+            SoundPlayer player = new SoundPlayer(Properties.Resources.sword2);
+            player.Play();
+            
+            Refresh();
+            Thread.Sleep(1000);
+            
+            playButton.Visible=true;
+            Refresh(); 
+        }
+
+
+
+
+
+    }
+}
